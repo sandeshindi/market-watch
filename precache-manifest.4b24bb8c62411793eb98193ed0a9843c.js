0ce0cd65dd7ef11a246f7dbbf1a28649318c938c
Sandesh <sandesh.indi@gmail.com>
2019-07-10 20:40:29 +0200

    Updates

---
+++ precache-manifest.4b24bb8c62411793eb98193ed0a9843c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1e69ed0896407ee2f05692852a54d7",
+    "url": "/market-watch/index.html"
+  },
+  {
+    "revision": "828ab0e55b5117928f98",
+    "url": "/market-watch/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "0a3bc0ca60f428f996c1",
+    "url": "/market-watch/static/css/main.8d83626d.chunk.css"
+  },
+  {
+    "revision": "828ab0e55b5117928f98",
+    "url": "/market-watch/static/js/2.2f3aba3f.chunk.js"
+  },
+  {
+    "revision": "0a3bc0ca60f428f996c1",
+    "url": "/market-watch/static/js/main.787231c6.chunk.js"
+  },
+  {
+    "revision": "90d2e0a70797d9b69491",
+    "url": "/market-watch/static/js/runtime~main.dac81ec8.js"
+  }
+]);
